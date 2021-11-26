ec5a9136a0b6c1a33b5d3ec94487d5adad3fd8f3
yuspring <springfish.0622@gmail.com>
2021-11-26 23:40:19 +0800

    11/26

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const fs = require('fs');
 const utils = require('util');
 require('dotenv').config();
 
+const { prefix } = require('./config.json');
+const client = new Discord.Client('./client/client.js');
+
 //--------------------------------------------------------------------------------
 
 
@@ -18,13 +21,11 @@ app.get('/', function(_request, response) {
 }).listen(app.get('port'), function() {
     console.log('App is running, server is listening on port', app.get('port'));
 });
+
 //--------------------------------------------------------------------------------
 
-const { prefix } = require('./config.json');
-const introduce=require('./bobotou-commands/introduce');
-const client = new Discord.Client('./client/client.js');
 
-
+//一般指令設定
 client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands');
 for (const file of commandFiles) {
@@ -32,21 +33,36 @@ for (const file of commandFiles) {
     client.commands.set(command.name, command);
 }
 
+//--------------------------------------------------------------------------------
 
-const bobotoucommandFiles = fs.readdirSync('./bobotou-commands');
+//波波頭指令設定
 client.bobocommands = new Discord.Collection();
+const bobotoucommandFiles = fs.readdirSync('./bobotou-commands');
 for (const file of bobotoucommandFiles) {
     const bobocommand = require(`./bobotou-commands/${file}`);
     client.bobocommands.set(bobocommand.name, bobocommand);
 }
 
+//--------------------------------------------------------------------------------
+
+
+//啟動時的console.log
 client.on("ready", async() => {
     console.log(`${client.user.username} 上線了 `);
 });
 
+//--------------------------------------------------------------------------------
 
+const trigger = ['機率', '波波波'];
+const cmdName = ['nick' , 'userinfo', 'cp'];
+
+//--------------------------------------------------------------------------------
+
+
+//主要指令
 
 client.on('message', async message => {
+
     const msg = message.content;
     const args = message.content.slice(prefix.length).split(/ +/);
     const commandName = args.shift().toLowerCase();
@@ -54,12 +70,12 @@ client.on('message', async message => {
 
     if (message.author.bot) return;
 
-    if(msg.indexOf('機率') != -1){
-        message.channel.send(Math.floor(Math.random()*100) + '%');
+    if(msg.indexOf(trigger[0]) != -1){
+        message.channel.send(`${Math.floor(Math.random()*100)}%`);
         return;
     }
 
-    if (msg == "波波波") {
+    if (msg == trigger[1]) {
         //if (message.author.id != 325576441540378625) return;
         let filter = m => m.author.id == message.author.id
         message.channel.send("幹嘛").then(() => {
@@ -78,12 +94,10 @@ client.on('message', async message => {
                     else{
                         client.bobocommands.get(args[0]).execute(message);
                     }
-                    
-
                 })
                 .catch(error => {
                     if (!utils.isError(error)) {
-                        message.channel.send(`<@${message.author.id}> 你為什麼沒打指令QQ`);
+                        message.channel.send(`<@${message.author.id}> 哼，不理你了`);
                     }
                 });
         })
@@ -92,12 +106,12 @@ client.on('message', async message => {
     if (!message.content.startsWith(prefix)) return;
 
     try {
-        if (commandName == 'nick' || commandName == 'userinfo' || commandName == 'cp') {
+        if (cmdName.includes(commandName)){
             command.execute(client, message);
-        } else {
+        }
+        else{
             command.execute(message);
         }
-
     } catch(error) {
         console.log(error);
     }
